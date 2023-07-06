d9e5c3a4017bb26ed93e4ee825e0fb1df647aa4c
DashbaordBuilder.net <support@dashboardbuilder.net>
2023-07-06 23:13:11 +0500

    add file

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * DashboardBuilder
+ *
+ * @author Diginix Technologies www.diginixtech.com
+ * Support <support@dashboardbuider.net> - https://www.dashboardbuilder.net
+ * @copyright (C) 2016-2023 Dashboardbuilder.net
+ * @version 6.3
+ * @license: Must read and agree LICENSE.txt before use.
+ */
+
+define('FOLDER', "../");
+define("DATA",FOLDER."data/");
+define("STORE",FOLDER."store/");
