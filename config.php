3e42ea97ef4779c2782489737a4ec343c23d83f9
DashbaordBuilder.net <support@dashboardbuilder.net>
2024-05-01 22:44:13 +0500

    v7l.1 updated

--- config.php
+++ config.php
@@ -4,8 +4,8 @@
  *
  * @author Diginix Technologies www.diginixtech.com
  * Support <support@dashboardbuider.net> - https://www.dashboardbuilder.net
- * @copyright (C) 2016-2023 Dashboardbuilder.net
- * @version 6.3
+ * @copyright (C) 2016-2024 Dashboardbuilder.net
+ * @version 7.1
  * @license: Must read and agree LICENSE.txt before use.
  */
 
