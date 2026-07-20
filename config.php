7c7d1e65192ba193f46e13d5a268bfee36ab8813
DashbaordBuilder.net <support@dashboardbuilder.net>
2026-07-20 22:36:05 +0500

    Add files via upload

--- config.php
+++ config.php
@@ -4,8 +4,8 @@
  *
  * @author Diginix Technologies www.diginixtech.com
  * Support <support@dashboardbuider.net> - https://www.dashboardbuilder.net
- * @copyright (C) 2016-2023 Dashboardbuilder.net
- * @version 6.3
+ * @copyright (C) 2016-2025 Dashboardbuilder.net
+ * @version 7.3
  * @license: Must read and agree LICENSE.txt before use.
  */
 
